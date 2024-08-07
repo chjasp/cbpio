e9857ba370cc31649aa93c68286941d8f908d8ad
chjasp <ch.jasper1@gmail.com>
2024-08-07 08:06:52 +0200

    tailwind start

---
+++ 01-nextjs/components/Header.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+const Header = () => {
+  return (
+    <header className="py-8 xl: py-12 text-white">
+      <div className="container mx-auto">header</div>
+      Header
+    </header>
+  );
+};
+
+export default Header;
