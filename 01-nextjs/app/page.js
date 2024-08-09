1c142a77986a4c2b7f992b5e49727b213632540e
chjasp <ch.jasper1@gmail.com>
2024-08-09 21:46:09 +0200

    homepage styling

--- 01-nextjs/app/page.js
+++ 01-nextjs/app/page.js
@@ -1,3 +1,39 @@
-export default function Home() {
-  return <main>Cloud Blueprints</main>;
-}
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+
+const Home = () => {
+  return (
+    <section className="container mx-auto h-full">
+      <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+        {/* text */}
+        <div className="text-center xl:text-left">
+          <span className="text-xl">Software Developer</span>
+          <h1 className="h1 mb-6">
+            Hello I'm <br />{" "}
+            <span className="text-accent">Christoph Jasper</span>
+          </h1>
+          <p className="max-w-[500px] mb-9 text-white/80">
+            I excel at crafting elegant digital experiances and I am proficient
+            in various programming languages and technologies.
+          </p>
+          {/* button and socials */}
+          <div>
+            <Button
+              variant="outline"
+              size="lg"
+              className="uppercase flex items-center gap-2"
+            >
+              <span>Download CV</span>
+              <FiDownload className="text-xl" />
+            </Button>
+            <div></div>
+          </div>
+        </div>
+        {/* photo */}
+        <div>photo</div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
